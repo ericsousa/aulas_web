5cae1320b106e5c0c1c7c376bc3e926578bc30ed
Eric Sousa <ericsousa@gmail.com>
2026-02-19 22:04:58 -0300

    tipos de variaveis

---
+++ lab03/src/teste/variaveisTipos.ts
@@ -0,0 +1,15 @@
+let nome: string = "Eric"
+let idade: number = 37
+let ativo: boolean = true
+
+let dado: any = 10
+let vazio: null = null
+let indefinido: undefined = undefined 
+
+// Tipo literais (valores  especificos)
+let situacao: "ativo" | "inativo" = "ativo" //só aceitas esses 2 valores
+
+// União de tipos (Union Type)
+let idadeOuNulu: number | null = null;
+
+console.log(`Nome: ${nome}, Idade: ${idade}, Ativo ${ativo}`)
